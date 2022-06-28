bf76a3b6e8e4e8ef70bcfd6bbdf36d70525c6310
Filolia <filolia@saintly.com>
2022-06-28 11:42:07 +0000

    dumpyara: specify git idenntity
    Signed-off-by: Filolia <filolia@saintly.com>

--- dumper.sh
+++ dumper.sh
@@ -1009,8 +1009,8 @@ rm -rf "${TMPDIR}" 2>/dev/null
 
 if [[ -s "${PROJECT_DIR}"/.github_token ]]; then
 	GITHUB_TOKEN=$(< "${PROJECT_DIR}"/.github_token)	# Write Your Github Token In a Text File
-	[[ -z "$(git config --get user.email)" ]] && git config user.email "DroidDumps@github.com"
-	[[ -z "$(git config --get user.name)" ]] && git config user.name "DroidDumps"
+	git config --global user.name "DroidDumps"
+	git config --global user.email "DroidDumps@github.com"
 	if [[ -s "${PROJECT_DIR}"/.github_orgname ]]; then
 		GIT_ORG=$(< "${PROJECT_DIR}"/.github_orgname)	# Set Your Github Organization Name
 	else
